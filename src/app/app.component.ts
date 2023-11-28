718cd1e7882e74fa67aedcbd2786c680ebcd1b48
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-11-28 13:56:16 -0800

    Todo with Angular Pt.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TodoListComponent } from "./todo-list/todo-list.component";
 import { FormsModule } from '@angular/forms';
+import { Todo2Component } from "./todo-2/todo-2.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, TodoListComponent, FormsModule]
+    imports: [CommonModule, RouterOutlet, TodoListComponent, FormsModule, Todo2Component]
 })
 export class AppComponent {
   title = 'app-name';
--- src/app/todo-2/todo-2.component.ts
+++ src/app/todo-2/todo-2.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-2',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo-2.component.html',
   styleUrl: './todo-2.component.css'
 })
 export class Todo2Component {
+
+  listArray =[{taskName:'Eat' , isCompleted: true}]
+
+  onSubmit(form: NgForm){
+    this.listArray.push({taskName: form.controls['task'].value ,isCompleted:false})
+  }
+
+  onDelete(index: number){
+    this.listArray.splice(index,1)
+  }
 
 }
